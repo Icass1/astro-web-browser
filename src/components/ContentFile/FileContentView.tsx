367ee05c11d4a88fad7984753ded7d51a77620f6
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-09-02 12:37:30 +0200

    Added video file view

--- src/components/ContentFile/FileContentView.tsx
+++ src/components/ContentFile/FileContentView.tsx
@@ -7,7 +7,6 @@ export default function FileContentView(
     {
         path,
         fileContent,
-        stat,
         isText,
         fileExt,
         fileType,
@@ -15,39 +14,45 @@ export default function FileContentView(
         {
             path: string | undefined,
             fileContent: Buffer | undefined,
-            stat: Stats | undefined,
             isText: boolean | undefined,
             fileExt: string | undefined,
             fileType: string | undefined,
         }) {
 
-    console.log(path)
 
     const renderMainView = () => {
+
         if (isText) {
             return <TextFileView fileContent={Uint8ArrayToStr(fileContent as Buffer)} />
         } else if (fileType?.startsWith("image")) {
             return (
                 <div className="h-full w-full overflow-auto">
-
-                    <img src={"/file/" + path} />
+                    {/* <img className="left-1/2 relative top-1/2 -translate-x-1/2 -translate-y-1/2" src={"/file/" + path} /> */}
+                    <img className="left-1/2 relative -translate-x-1/2" src={"/file/" + path} />
                 </div>
             )
         } else if (fileType == "application/pdf") {
             return (
                 <iframe className="h-full w-full" src={"/file/" + path}></iframe>
-
+            )
+        } else if (fileType == "video/mp4") {
+            return (
+                <video className="max-h-full max-w-full" controls src={"/file/" + path}></video>
+            )
+        } else {
+            return (
+                <>
+                    <label>File format not supported</label>
+                    <label>fileType '{fileType}'</label>
+                    <label>isText '{JSON.stringify(isText)}'</label>
+                </>
             )
         }
     }
 
     return (
-        <div className="flex flex-col gap-10 h-full overflow-hidden px-2 pb-4">
-            {fileContent ?
-                renderMainView()
-                :
-                <div>No content</div>
-            }
+        <div className="flex flex-col gap-10 h-full overflow-hidden px-1 pb-2">
+            {renderMainView()}
         </div>
     )
 }
