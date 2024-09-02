fbc47caa09ab1a95df09e9e35591978153d83f6f
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-09-02 17:07:51 +0200

    Changed upload to XHR to track progress

--- src/lib/uploadFile.ts
+++ src/lib/uploadFile.ts
@@ -1,7 +1,7 @@
 
 import { toast } from "sonner";
 
-export async function uploadFile(path: string, file: File) {
+export async function oldUploadFile(path: string, file: File) {
 
     toast(`Uploading '${file.name}'`)
 
@@ -26,5 +26,37 @@ export async function uploadFile(path: string, file: File) {
     } catch (error) {
         toast(`'${file.name}' couldn't be uploaded`, { style: { color: '#ed4337' } })
     }
+}
 
+export async function uploadFile(path: string, file: File) {
+
+    toast(`Uploading '${file.name}'`)
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('path', path);
+
+    const xhr = new XMLHttpRequest();
+    const success = await new Promise((resolve) => {
+        xhr.upload.addEventListener("progress", (event) => {
+            if (event.lengthComputable) {
+                console.log("upload progress:", event.loaded / event.total);
+                // uploadProgress.value = event.loaded / event.total;
+                console.log(event.loaded / event.total)
+            }
+        });
+        xhr.addEventListener("progress", (event) => {
+            if (event.lengthComputable) {
+                console.log("download progress:", event.loaded / event.total);
+                console.log(event.loaded / event.total)
+                // downloadProgress.value = event.loaded / event.total;
+            }
+        });
+        xhr.addEventListener("loadend", () => {
+            resolve(xhr.readyState === 4 && xhr.status === 200);
+        });
+        xhr.open("POST", '/api/upload-file', true);
+        // xhr.setRequestHeader("Content-Type", "application/octet-stream");
+        xhr.send(formData);
+    });
 }
