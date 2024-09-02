df456f115da0d6eb24e275990a079853afd41aa5
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-09-02 17:08:56 +0200

    Added rename api route

---
+++ src/pages/api/rename-file.ts
@@ -0,0 +1,47 @@
+import type { APIContext } from "astro";
+
+import * as fs from 'fs/promises';
+import * as path from 'path';
+
+export async function POST(context: APIContext): Promise<Response> {
+
+    if (!context.locals.user) {
+        return new Response(
+            JSON.stringify({
+                error: "User not logged in"
+            }),
+            {
+                status: 401
+            }
+        );
+    }
+
+    const data = await context.request.json()
+    try {
+        const src = path.join(context.locals.user.scope, data.oldPath)
+        const dest = path.join(context.locals.user.scope, data.newPath)
+        console.log(src, dest)
+
+        await fs.rename(src, dest)
+
+        return new Response(
+            JSON.stringify({
+                error: "OK"
+            }),
+            {
+                status: 200
+            }
+        );
+    } catch (error) {
+        console.log(error)
+        return new Response(
+            JSON.stringify({
+                error: "Error renaming file"
+            }),
+            {
+                status: 500
+            }
+        );
+    }
+
+}
