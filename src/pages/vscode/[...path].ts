45aaf8cf32365529d32ab582e038d8f696cfa8e1
icassLaptop <i.cassdolopez@gmail.com>
2024-09-29 20:16:06 +0200

    Started vscode api paths

---
+++ src/pages/vscode/[...path].ts
@@ -0,0 +1,22 @@
+
+import type { SqliteError } from "better-sqlite3";
+import { db } from "../../lib/db";
+
+import type { APIContext } from "astro";
+
+
+export async function POST(context: APIContext): Promise<Response> {
+
+    console.log(context)
+
+	return new Response("POST");
+}
+
+export async function GET(context: APIContext): Promise<Response> {
+
+    console.log(context)
+
+    const response = fetch("http://127.0.0.1:8080/?folder=/home/icass/HomeDrive/root")
+
+	return response
+}
