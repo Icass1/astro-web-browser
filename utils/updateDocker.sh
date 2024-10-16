17cec40ae3c9aa637b3c620533c10297d7219844
IgnacioMainUbuntu <i.cassdolopez@gmail.com>
2024-10-16 14:26:10 +0200

    Added HomeDrive path to docker run

--- utils/updateDocker.sh
+++ utils/updateDocker.sh
@@ -8,4 +8,4 @@ docker buildx build -t astro-web-browser -f Dockerfile .
 set +e
 docker stop WebBrowser
 docker rm WebBrowser
-docker run --name WebBrowser -d -t --restart unless-stopped -p 8081:4321 -v /home/icass/TestVolumeDelete:/mnt/main astro-web-browser
+docker run --name WebBrowser -d -t --restart unless-stopped -p 8081:4321 -v /home/icass/HomeDrive/root:/mnt/main astro-web-browser
