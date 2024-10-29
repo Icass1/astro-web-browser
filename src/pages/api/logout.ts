aee7584b1d3307de7a7a9121d1d0084dfe42bc5a
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-10-29 17:33:50 +0100

    Combined GET and POST into ALL

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -2,22 +2,7 @@ import { lucia } from "@/auth";
 
 import type { APIContext } from "astro";
 
-export async function POST(context: APIContext): Promise<Response> {
-    if (!context.locals.session) {
-        return new Response("Error", {
-            status: 401
-        });
-    }
-
-    await lucia.invalidateSession(context.locals.session.id);
-
-    const sessionCookie = lucia.createBlankSessionCookie();
-    context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-
-    return new Response("OK");
-}
-
-export async function GET(context: APIContext): Promise<Response> {
+export async function ALL(context: APIContext): Promise<Response> {
     if (!context.locals.session) {
         return new Response("error", {
             status: 401
