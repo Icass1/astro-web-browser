b7e8dbd8174fac06907020c3765d019d40c00832
IgnacioMainUbuntu <i.cassdolopez@gmail.com>
2024-11-06 23:17:04 +0100

    Changed docker user to icass

--- utils/updateDocker.sh
+++ utils/updateDocker.sh
@@ -8,7 +8,7 @@ docker buildx build -t astro-web-browser -f Dockerfile .
 set +e
 docker stop WebBrowser
 docker rm WebBrowser
-docker run --name WebBrowser -d -t --restart unless-stopped -p 8081:4321 -v /home/icass/HomeDrive/root:/mnt/main -v /home/icass/HomeDrive/files2database:/usr/src/app/database astro-web-browser
+docker run --name WebBrowser --user $(id -u icass):$(id -g icass) -d -t --restart unless-stopped -p 8081:4321 -v /home/icass/HomeDrive/root:/mnt/main -v /home/icass/HomeDrive/files2database:/usr/src/app/database astro-web-browser
 
 # Get the directory where the script is located
 SCRIPT_DIR="$(dirname "$0")"
